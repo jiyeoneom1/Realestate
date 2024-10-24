34131bf0084a6e5f4d8baa2b06abf875ffa23775
JIYEON EOM <76675678+jiyeoneom1@users.noreply.github.com>
2024-10-25 02:50:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
-sns.set(style="whitegrid")
-# plt.rcParams['font.family'] = 'NanumGothic'
-plt.rcParams['font.family'] = 'NanumBarunGothic'
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+
+# NanumBarunGothic 폰트 로드
+font_path = "./fonts/NanumBarunGothic.ttf"
+font_prop = fm.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = font_prop.get_name()
 
 # 페이지 설정
 st.set_page_config(
